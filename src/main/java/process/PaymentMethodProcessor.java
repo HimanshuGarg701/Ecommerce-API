6bc8616723c769b0a66e23f2e753c317d27adfdf
xo28122000 <jainam.2000@yahoo.com>
2020-03-12 15:11:14 -0700

    Update PaymentMethodProcessor.java

--- src/main/java/process/PaymentMethodProcessor.java
+++ src/main/java/process/PaymentMethodProcessor.java
@@ -1,6 +1,18 @@
 package process;
 
-import DAO.PaymentMethodDAO;
+//import DAO.PaymentMethodDAO;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
+import static com.mongodb.client.model.Filters.*;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import DTO.DTO;
 import DTO.PaymentMethodDTO;
 import DTO.ResponseDTO;
@@ -26,10 +38,21 @@ public class PaymentMethodProcessor implements Processor {
         rbh.setDate(new Date().toString());
         rbh.setParameters(arguments);
         try {
-            PaymentMethodDAO paymentmethoddao = PaymentMethodDAO.getInstance();
-            PaymentMethodDTO paymentMethod = new PaymentMethodDTO(paymentmethoddao.getIndex(), arguments.get("method"));
-            response.add(paymentMethod);
-            paymentmethoddao.addPaymentMethod(paymentMethod);
+//            PaymentMethodDAO paymentmethoddao = PaymentMethodDAO.getInstance();
+//            PaymentMethodDTO paymentMethod = new PaymentMethodDTO(paymentmethoddao.getIndex(), arguments.get("method"));
+//            paymentmethoddao.addPaymentMethod(paymentMethod);
+
+
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            MongoDatabase db = mongoClient.getDatabase("csc-415");
+            MongoCollection<Document> myColection = db.getCollection("PaymentMethod");
+            Document doc = new Document("name", arguments.get("method"));
+            myColection.insertOne(doc);
+
+
+
+
+//            response.add(paymentMethod);
             responseCode = "OK";
         } catch (Exception e) {
             responseCode = "Error";
@@ -37,5 +60,14 @@ public class PaymentMethodProcessor implements Processor {
         rbh.setResponseCode(responseCode);
         rbh.setResponse(response);
         return rbh.build();
+    }
+
+
+    public static void main(String[] args) {
+        HashMap<String, String> arg = new HashMap<String, String>();
+        arg.put("method","blah");
+        PaymentMethodProcessor p = new PaymentMethodProcessor(arg);
+        p.process();
+
     }
 }
